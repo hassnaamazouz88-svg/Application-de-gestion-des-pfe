5ba14647feca42f24269ff87b92f630506f95b45
hassnaamazouz88-svg <hassnaamazouz88@gmail.com>
2026-05-09 22:04:19 +0100

    update feature

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=gestion_pfe",
+    "root",
+    ""
+);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+return $pdo;
+
+?>
