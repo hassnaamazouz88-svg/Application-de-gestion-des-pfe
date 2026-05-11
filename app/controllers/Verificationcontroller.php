f90f3428d6c28fb5740f3212fb746328901732c5
Ridani <oumaymaridani@gmail.com>
2026-05-11 02:16:00 +0200

    Ajout interfaces dashboard planning

--- app/controllers/Verificationcontroller.php
+++ app/controllers/Verificationcontroller.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../services/VerificationService.php';
+require __DIR__ . '/../views/verification.view.php';
 
 class VerificationController
 {
---
+++ planning.php
@@ -0,0 +1,3 @@
+<?php
+
+require 'app/views/planning.view.php';
